14e9b8d70fda8c5ff8e9d28ea0cb894cbf0a34f0
Deathsquad08 <stanton.fraser08@gmail.com>
2017-04-17 12:11:49 -0500

    7.2 Changes.  Fire & Frost

--- rotations/mage/fire.lua
+++ rotations/mage/fire.lua
@@ -1,73 +1,122 @@
 -- SPEC ID 63
-NetherMachine.rotation.register(63, {
+NetherMachine.rotation.register_custom(63, '|cff69ACC8Legion 7.2 Fire Mage|r', {
+-- PLAYER CONTROLLED: Temporal Shield, Blizzard, Blink, Frost Bomb
+-- SUGGESTED TALENTS:
+-- CONTROLS: Pause - Left Control
+-- 48108 Hot Streak!
+-- 48107 Heating Up
 
-  --------------------
-  -- Start Rotation --
-  --------------------
-  
-  -- Cooldowns
-  { "45438", {
-    "modifier.cooldowns", 
-    "player.health <= 30" } }, -- Ice Block
-  { "11958", {
-    "modifier.cooldowns", 
-    "player.health <= 25", 
-    "player.spell(45438).cooldown",
-  }},-- Cold Snap
-  
-  { "55342", "modifier.cooldowns" }, -- Mirror Image
-  { "108978", "modifier.cooldowns" }, -- Alter Time
-  { "12043", "modifier.cooldowns" }, -- Presence of Mind
-  { "115610", { 
-    "modifier.cooldowns", 
-    "player.health <= 80",
-  }},-- Temporal Shield
-  
-    -- Interrupts
-  { "2139", "modifier.interrupts" }, -- Counterspell
-  { "102051", "modifier.interrupts" }, -- Frostjaw
-  
-  -- AoE
-  --{ "10", "modifier.ctrl", "ground" }, -- Blizzard -- Unimplemented 
-  { "2120", "modifier.control", "ground" }, -- Flamestrike
-  { "113724", "modifier.alt", "ground" }, -- Ring of Frost
+-- { "11366"},
+
+--
+{ "pause", "player.buff(Ice Block)"},
+{ "pause", { "player.buff(Kael'thas's Ultimate Ability)", "player.casting(Pyroblast)"}},
+{ "Pyroblast", { "player.buff(Kael'thas's Ultimate Ability)", "!player.buff(48108)", "!modifier.last(Pyroblast)"}},
+{ "Meteor", { "modifier.shift", "!player.moving" }, "target.ground"},
+{ "Flamestrike", { "modifier.shift", "player.buff(48108)" }, "target.ground"},
+{ "Pyroblast",    "player.buff(48108)"},
+
+{ "#trinket1", "toggle.trinkets" },
+{ "#trinket2", "toggle.trinkets" },
+{ "Counterspell", "modifier.interrupts" },
+{ "Living Bomb", {"!target.buff(Living Bomb)", "!player.buff(48108)", "!player.buff(48107)", "!player.buff(Kael'thas's Ultimate Ability)", "modifier.multitarget"}},
+
+{ "Combustion", "toggle.burn"},
+{ "Combustion", "player.buff(Rune of Power)"},
+{ "Mirror Image", "modifier.cooldowns"},
+
+{ "Pyroblast",   { "player.buff(48108)", "!player.lastcast(Pyroblast)"}},
+{ "Flame On", {"player.spell(Fire Blast).charges < 1", "modifier.cooldowns"}},
+{ "Fire Blast",  { "player.buff(48107)", "!player.lastcast(Fire Blast)", "!player.lastcast(Phoenix's Flames)"}},
+{ "Phoenix's Flames", {"player.buff(48107)", "player.spell(Fire Blast).charges < 1", "!player.lastcast(Phoenix's Flames)", "toggle.flames"}},
+--{ "Phoenix's Flames", { "!player.buff(48108)", "player.spell(Combustion).cooldown > 45"}},
+{ "/run CastSpellByName('Fire Blast');", {"player.casting.percent <= 100", "player.buff(48107)", "player.spell(Fire Blast).charges > 0"  }},
+{ "/run RunMacro('sdf');", {"player.casting.percent <= 100", "player.buff(48108)"  }},
+{ "/run RunMacro('sdf');", {"player.casting.percent <= 100", "player.buff(48107)", "player.spell(Fire Blast).charges > 0", "player.spell(Phoenix's Flames).charges > 0"   }},
+{ "Spellsteal", "target.stealable", "target" 						},
+{ "Ice Floes", {"player.moving", "!player.buff(Ice Floes)"}},
+{ "Scorch", {"player.moving","!player.buff(Ice Floes)", "!player.buff(48108)" }},
+
+
+--{-- "/cast Pyroblast", {"player.casting.percent <= 100", "player.buff(48107)", "!player.lastcast(Pyroblast)", "target" }},
+--{ --"/cast Fire Blast", { "player.buff(48107)", "player.casting"}},
+--{ --"/cast Pyroblast",  { "player.buff(48108)", "player.casting"}},
+
+
+-- Working ok from here 
+--{ "/cast Pyroblast", { "player.buff(48108)", function() local s,_=UnitChannelInfo("player"); return s and s == 'Fireball' end } }, 
+--{ "/cast Fire Blast", { "player.buff(48107)", function() local s,_=UnitChannelInfo("player"); return s and s == 'Fireball' end } }, 
+
+{ "Rune of Power", {"!player.buff(Rune of Power)", "modifier.cooldowns"}},
+
+{ "Blazing Barrier", "player.health < 50"},
+
+--{ "/run CastSpellByName('Pyroblast');", { "player.buff(Hot Streak!)", "player.casting" } },
+--{ "Pyroblast", { "player.buff(Hot Streak!)", "player.casting"}},
+--{ "/run CastSpellByName('Fire Blast');", { "player.buff(48107)", "player.casting" } },
+--{ "Fire Blast", { "player.buff(48107)", "player.casting"}},
+--{ "Phoenix's Flames", {"player.buff(Rune of Power)", "player.spell(Fire Blast).charges < 1"}},
+--{ "Pyroblast", {"player.buff(Hot Streak!)", "!lastcast(Pyroblast)"}},
+--{ "Fire Blast", "player.buff(48107)"}, -- Heating Up
+{ "Dragon's Breath", { "!player.buff(48108)", "!player.buff(48107)","target.range <= 10", "toggle.dragons", "talent(4, 1)"}},
+{ "Cinderstorm", {"target.debuff(Ignite)", "toggle.cinderstorm"}},
+--{ "/run RunMacro('sdf');", {"player.lastcast(Pyroblast)", "!player.buff(Kael'thas's Ultimate Ability)"}},
+--{ "Pyroblast", { "player.buff(Kael'thas's Ultimate Ability)", "!player.buff(48108)", "!lastcast(11366)"}},
+--{ "/run RunMacro('sdf');", {"player.casting(Pyroblast)", "!player.buff(Kael'thas's Ultimate Ability)" }},
+{ "Scorch", { "target.health < 30", "toggle.usescorch"}},
+{ "Fireball", "!player.buff(Combustion)" },
+{ "Fireball", "player.buff(Combustion)"},
+
+
+--{ "/run RunMacro('sdf');", {"player.casting.percent <= 100", "player.buff(48107)", "player.spell(Fire Blast).charges > 0"  }},
+--{ "Fire Blast" , "player.spell(Fire Blast).charges > 1" },
+
+--[[
+	{ "Pyroblast", {"player.buff(Hot Streak!)", "player.casting"}},
+	{ "Pyroblast", "player.buff(Hot Streak!)"},
+
+	{ "/run CastSpellByName('Fire Blast');", { "player.buff(Heating Up)", "player.casting" } },
+	{ "/run CastSpellByName('Fire Blast');", "player.buff(Heating Up)" },
+	{ "Pyroblast", "lastcast(Fire Blast)"},
+	{ "108853", "player.buff(Heating Up)",  "player.casting"}, -- "!lastcast",
+	{ "Fire Blast", {"player.buff(Heating Up)",  "!lastcast"}},
+
+	{ "Fire Blast", {"modifier.interrupts", "player",  },
+
+	{ "Flame On", "player.spell(Fire Blast).charges < 1"},
+
+	{ "Fire Blast", {"player.buff(Heating up)", "!lastcast"}},
+	{ "Blast Wave", {"lastcast(Fireball)", "!player.buff(Heating Up)"}},
 	
-  -- Mage Bombs
-  { "114923", "!target.debuff(114923)", "target" }, -- Nether Tempest
-  { "114923", "target.debuff(114923).duration <= 2", "target" }, -- Nether Tempest
-  { "44457", "!target.debuff(44457)", "target" }, -- Living Bomb
-  { "112948", "player.spell(112948).cooldown = 0", "target" }, -- Frost Bomb
-  
-  -- Survivability
-  { "122", "target.range <= 9" }, -- Frost Nova
-  { "1953", "target.range <= 3" }, -- Blink
-  { "31661", "target.range <= 5" }, -- Dragon's Breath
-  { "116011", "modifier.shift", "ground" }, -- Rune of Power
-  { "11426", "player.health <= 80" }, -- Ice Barrier
-  
-  -- Rotation
-  { "11129",  "target.debuff(12654)" }, -- Combustion
-  { "11366", "player.buff(48108)" }, -- Pyroblast
-  { "108853", "player.buff(48107)" }, -- Inferno Blast
-  { "44457", "target.debuff(44457).duration <= 3" }, -- Living Bomb
-  { "2948", "player.moving" }, -- Scorch
-  { "133" } -- Fireball
-
+	{ "Fireball", "!player.buff(Heating Up)"},
+--]]
+-- ability_mage_moltenarmor
   ------------------
   -- End Rotation --
   ------------------
+},{
+	-- OUT OF COMBAT
+{ "Blazing Barrier", {"!player.buff(Blazing Barrier)", "player.moving", "toggle.bubble"}},
+{ "Slow Fall", {"!player.buff(Slow Fall)", "toggle.slowfall", "player.falling"}},
+{  "/cast Combustion", { "player.casting", "!player.buff(48107)", "!player.buff(48108)", "player.casting.percent <= 10", "modifier.cooldowns", "toggle.raiding" }},
+},
+function()
+  NetherMachine.toggle.create('burn', 'Interface\\ICONS\\spell_fire_sealoffire', 'burn', 'Combustion will be used')
   
-  },{
-  
-  ---------------
-  -- OOC Begin --
-  ---------------
-  
-  { "1459", "!player.buff(1459).any" }, -- Arcane Brilliance
-  { "30482", "!player.buff(30482)" }
-  
-  -------------
-  -- OOC End --
-  -------------
-  
-  }) -- Molten Armor
+  NetherMachine.toggle.create('flames', 'Interface\\ICONS\\artifactability_firemage_phoenixbolt', 'Use Flames!', 'Phoenix Flames Will be Used')
+  NetherMachine.toggle.create('usescorch', 'Interface\\ICONS\\Spell_fire_soulburn', 'Use Scorch', 'If you have the Lego Waist use this')
+  NetherMachine.toggle.create('cinderstorm', 'Interface\\ICONS\\spell_fire_flare', 'Use Cinderstorm!', 'Turn off if your pulling to many adds')
+  NetherMachine.toggle.create('dragons', 'Interface\\ICONS\\inv_misc_head_dragon_01', 'Use Dragons Breath', 'Turn on when talented with Dragons Breath')
+  NetherMachine.toggle.create('raiding', 'Interface\\ICONS\\Achievement_boss_general_nazgrim', 'PreCast', 'Will Combust on Pull')
+  NetherMachine.toggle.create('trinkets', 'Interface\\ICONS\\sha_spell_fire_bluepyroblast_nightmare', 'Trinkets', 'Use your Trinkets on CD')
+  NetherMachine.toggle.create('slowfall', 'Interface\\ICONS\\spell_magic_featherfall', 'Slow Fall', 'Auto Slow Fall')
+   NetherMachine.toggle.create('bubble', 'Interface\\ICONS\\ability_mage_moltenarmor', 'bubble', 'Blazing Barrier will now be used when not in combat')
+end)
+
+
+
+
+
+
+
+
